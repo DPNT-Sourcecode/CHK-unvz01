4da22a195a6bb183634a1e8effe0c873ad9c5cd4
sbx_user1051 <sbx_user1051@169.254.211.209>
2022-04-06 20:36:30 +0000

    Wed Apr 06 20:36:30 UTC 2022

--- test/solutions/sum_solutions.py
+++ test/solutions/sum_solutions.py
@@ -2,14 +2,14 @@ class sum_solution():
 
     def compute(self,x,y):
         if ((x >= 0 and x <=100) and
-             y >= 0 and y <=100):  
+            (y >= 0 and y <=100)):  
             return int(x+y)
         else:
             print(('Parameters must be positive integers' 
                    + 'greater than 0 and lower than 100'))
             return False
 
-
 class one():
     def get(self):
         return 1
+
