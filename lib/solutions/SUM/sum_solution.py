13691bd6de6645358bc3881dba8890e64cb85ec3
sbx_user1051 <sbx_user1051@169.254.55.173>
2022-04-07 21:56:59 +0000

    Thu Apr 07 21:56:59 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(self,x,y):
+def compute(x,y):
     if ((x >= 0 and x <=100) and
         (y >= 0 and y <=100)):  
         return int(x+y)
     else:
         raise Exception('x and y must be integers between 0-100')
+
 
 
