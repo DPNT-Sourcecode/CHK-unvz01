5cc547fd5322dab3169524c7729ab739dde639ec
sbx_user1051 <sbx_user1051@169.254.193.41>
2022-04-08 16:39:38 +0000

    Fri Apr 08 16:39:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,9 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from os import O_ACCMODE
+
+
 def checkout(skus):
     prices = {
         'A':50,
@@ -10,14 +13,24 @@ def checkout(skus):
         'D':15,
     }
     total = 0
-    for item in prices.item():
+    for item in prices.items():
         occurrences = skus.count(item[0])
         if item[0] == 'A':
             if occurrences >= 3:
-                total += 130 * (occurrences // 3) + (prices[item[0]]*occurrences%3)
+                total += 130 * (occurrences // 3) + (prices[item[0]] * (occurrences%3))
             else:
                 total += occurrences*prices[item[0]]
-        if item[0] == 'B':
+        elif item[0] == 'B':
             if occurrences >= 2:
-                total += 45 * (occurrences//2)
+                total += 45 * (occurrences//2) + (prices[item[0]] * (occurrences%2))
+            else:
+                total += occurrences*prices[item[0]]
+        elif item[0] == 'C':
+            total += occurrences*prices[item[0]]
+        elif item[0] == 'D':
+            total += occurrences*prices[item[0]]
+        else:
+            return -1  
+    return total
+
 
