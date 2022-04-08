f92f29f5a9f0e539059459804f3094b6b2fb64f5
sbx_user1051 <sbx_user1051@169.254.193.41>
2022-04-08 16:36:38 +0000

    Fri Apr 08 16:36:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,4 +9,15 @@ def checkout(skus):
         'C':20,
         'D':15,
     }
-    
+    total = 0
+    for item in prices.item():
+        occurrences = skus.count(item[0])
+        if item[0] == 'A':
+            if occurrences >= 3:
+                total += 130 * (occurrences // 3) + (prices[item[0]]*occurrences%3)
+            else:
+                total += occurrences*prices[item[0]]
+        if item[0] == 'B':
+            if occurrences >= 2:
+                total += 45 * (occurrences//2)
+
