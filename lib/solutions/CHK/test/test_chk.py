f7b10040fd85ad455ae00ddadd6077e2fc7b67a9
sbx_user1051 <sbx_user1051@169.254.193.41>
2022-04-08 17:32:52 +0000

    Fri Apr 08 17:32:52 UTC 2022

--- lib/solutions/CHK/test/test_chk.py
+++ lib/solutions/CHK/test/test_chk.py
@@ -9,3 +9,12 @@ def test_basket_2():
 
 def test_basket_3():
     assert chk.checkout('ABCDF') == -1
+
+def test_basket_4():
+    assert chk.checkout('ABCDF') == -1
+
+def test_basket_5():
+    assert chk.checkout('A') == -1
+
+def test_basket_6():
+    assert chk.checkout('ABCDF') == -1
